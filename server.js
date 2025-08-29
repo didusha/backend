5efeab4959dcd074321831157e0ec98e000d4c68
Itaish-Bzu <Itaish94@gmail.com>
2025-08-29 12:53:22 +0300

    Add cors option

--- server.js
+++ server.js
@@ -26,7 +26,9 @@ if (process.env.NODE_ENV === 'production') {
         origin: [   'http://127.0.0.1:3030',
                     'http://localhost:3030',
                     'http://127.0.0.1:5173',
-                    'http://localhost:5173'
+                    'http://localhost:5173',
+                    'http://127.0.0.1:5174',
+                    'http://localhost:5174'
                 ],
         credentials: true
     }
